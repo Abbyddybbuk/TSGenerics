98b6365386bdafb3c6ee26eea8aeaa82134b07e9
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2022-06-13 01:06:08 +0100

    KeyOf Constraint

--- src/app.ts
+++ src/app.ts
@@ -12,39 +12,71 @@
 // });
 
 function merge<T, U>(objA: T, objB: U) {
-   return Object.assign(objA, objB);
+  return Object.assign(objA, objB);
 }
 
-const mergedObj = merge({name: 'Abhijeet', profession: 'Software Engineer', hobbies: ['Documentaries', 'Gaming', 'Driving']}, {carType: 'SUV'});
+const mergedObj = merge(
+  {
+    name: "Abhijeet",
+    profession: "Software Engineer",
+    hobbies: ["Documentaries", "Gaming", "Driving"],
+  },
+  { carType: "SUV" }
+);
 console.log(mergedObj.profession);
 
-const mergedObj2 = merge({surname: 'Kulshreshtha', city: 'Gwalior', address: ['G1', 'G2', 'G3']}, {color: 'Wheatish'});
+const mergedObj2 = merge(
+  { surname: "Kulshreshtha", city: "Gwalior", address: ["G1", "G2", "G3"] },
+  { color: "Wheatish" }
+);
 console.log(mergedObj2.surname);
 
-function merge2<T extends Object, U extends Object>(objA: T, objB: U) {// Here T can only be an object and so do U also you can T extends string|number
-    return Object.assign(objA, objB);
+function merge2<T extends Object, U extends Object>(objA: T, objB: U) {
+  // Here T can only be an object and so do U also you can T extends string|number
+  return Object.assign(objA, objB);
 }
 
-const mergedObj3 = merge({name: 'Abhijeet', profession: 'Software Engineer', hobbies: ['Documentaries', 'Gaming', 'Driving']}, {carType: 'SUV'});
+const mergedObj3 = merge(
+  {
+    name: "Abhijeet",
+    profession: "Software Engineer",
+    hobbies: ["Documentaries", "Gaming", "Driving"],
+  },
+  { carType: "SUV" }
+);
 console.log(mergedObj.profession);
 
-const mergedObj4 = merge({surname: 'Kulshreshtha', city: 'Gwalior', address: ['G1', 'G2', 'G3']}, {color: 'Wheatish'});
+const mergedObj4 = merge(
+  { surname: "Kulshreshtha", city: "Gwalior", address: ["G1", "G2", "G3"] },
+  { color: "Wheatish" }
+);
 console.log(mergedObj2.surname);
 
 interface Lengthy {
-    length: number;
+  length: number;
 }
 
 function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-    let descriptionText = 'Got No Value';
-    if (element.length === 1) {
-      descriptionText = 'Got 1 Element';
-    } else if (element.length > 1) {
-      descriptionText = 'Got ' + element.length + ' elements.';
-    }
+  let descriptionText = "Got No Value";
+  if (element.length === 1) {
+    descriptionText = "Got 1 Element";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
 
-    return [element, descriptionText];
+  return [element, descriptionText];
 }
 
-// console.log(countAndDescribe(['Yahoo']));
+console.log(countAndDescribe(["Yahoo"]));
 // console.log(countAndDescribe(10));This is not possible because number does not have length property
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  //It means U has to be key of T
+  return "Value: " + obj[key];
+}
+
+extractAndConvert({ name: "Abhijeet" }, "name"); // if you do not pass name as 2nd parameter then you will get an error as 2nd parameter has toe be property of object passed
+// in first param
